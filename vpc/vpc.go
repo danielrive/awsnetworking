391cfec96e7590fa5c1e5a205a57828d18490a65
danielrivert <danielgerman.rivera@globant.com>
2020-11-02 22:40:10 -0500

    RT added and IGW

--- vpc/vpc.go
+++ vpc/vpc.go
@@ -5,15 +5,20 @@ import (
 	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"
 )
 
+// Basic information to create AWS VPC
 type Vpcinfo struct {
-	Name string
-	CIDR string
+	Name      string
+	CIDR      string
+	EnableNAT bool
 }
 
+// Create vpc with route public and private route tables, internet gateway and NAT GW (optional)
 func Createvpc(info Vpcinfo) {
 	pulumi.Run(func(ctx *pulumi.Context) error {
-		_, err := ec2.NewVpc(ctx, info.Name, &ec2.VpcArgs{
-			CidrBlock: pulumi.String(info.CIDR),
+		vpc, err := ec2.NewVpc(ctx, info.Name, &ec2.VpcArgs{
+			CidrBlock:          pulumi.String(info.CIDR),
+			EnableDnsHostnames: pulumi.BoolPtr(true),
+			EnableDnsSupport:   pulumi.BoolPtr(true),
 			Tags: pulumi.StringMap{
 				"Name":        pulumi.String(info.Name),
 				"CreatedBu4y": pulumi.String("Pulumi"),
@@ -22,6 +27,37 @@ func Createvpc(info Vpcinfo) {
 		if err != nil {
 			return err
 		}
-		return nil
+
+		//creating Internet GW
+		IGW, err := ec2.NewInternetGateway(ctx, "IGW"+info.Name, &ec2.InternetGatewayArgs{
+			VpcId: pulumi.String(vpc.Id),
+			Tags: pulumi.StringMap{
+				"Name":        pulumi.String("IGW" + info.Name),
+				"CreatedBu4y": pulumi.String("Pulumi"),
+			},
+		})
+		if err != nil {
+			return err
+		}
+
+		// Creating Route tables, public and private
+
+		_, err := ec2.NewRouteTable(ctx, "PublicRT"+info.Name, &ec2.RouteTableArgs{
+			VpcId: pulumi.String(vpc.Id),
+			Routes: ec2.RouteTableRouteArray{
+				&ec2.RouteTableRouteArgs{
+					CidrBlock: pulumi.String("0.0.0.0/0"),
+					GatewayId: pulumi.String(IGW.Id),
+				},
+			},
+			Tags: pulumi.StringMap{
+				"Name":        pulumi.String("PublicRT" + info.Name),
+				"CreatedBu4y": pulumi.String("Pulumi"),
+			},
+		})
+		if err != nil {
+			return err
+		}
+
 	})
 }
